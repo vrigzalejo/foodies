11a52c058b77d00cde4998232831c9e82e0d9dad
Vrigz Alejo <brigsalejo@gmail.com>
2024-08-11 22:38:46 +0800

    done on 109

--- app/meals/layout.js
+++ app/meals/layout.js
@@ -1,6 +1,5 @@
 export default function MealsLayout({children}) {
     return <>
-        <p>Meals Layout</p>
         {children}
     </>
 }
--- app/meals/page.js
+++ app/meals/page.js
@@ -2,7 +2,11 @@ import MealsGrid from '@/components/meals/meals-grid';
 import classes from './page.module.css'
 import Link from 'next/link';
 
-export default function MealsPage() {
+import { getMeals } from '@/lib/meals'
+
+export default async function MealsPage() {
+  const meals = await getMeals()
+
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +24,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
