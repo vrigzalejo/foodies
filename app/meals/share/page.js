b34346070d25eb50c0319189dacb6ae013228150
Vrigz Alejo <brigsalejo@gmail.com>
2024-10-18 18:40:23 +0800

    updated the invalid input UI in share

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
 'use client';
 
+import { useFormState } from 'react-dom';
+
 import ImagePicker from '@/components/meals/image-picker';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import { shareMeal } from '@/lib/actions';
@@ -7,6 +9,8 @@ import { shareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -16,7 +20,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,6 +44,7 @@ export default function ShareMealPage() {
             <textarea id="instructions" name="instructions" rows="10" required></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
